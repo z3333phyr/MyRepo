b3b5a47f3c24ad90a0c62a6a1439369ccff8baa0
z3333phyr <pagulayan.francis03@gmail.com>
2025-04-21 18:40:06 +0800

    file transfer

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,12 @@
+
+import generateName from "sillyName";
+
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
+
+
+import superheroes from "superheroes";
+
+const name = superheroes.random();
+console.log(`I am ${name}!`);
