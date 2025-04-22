0e7ec2e595e79496523b63d8e4c8f9c10243f118
z3333phyr <pagulayan.francis03@gmail.com>
2025-04-22 09:08:12 +0800

    modified

--- Backend/8.2 Postgres Read/index.js
+++ Backend/8.2 Postgres Read/index.js
@@ -16,16 +16,14 @@ const port = 3000;
 db.connect();
 
 let quiz = [];
-db.query("SELECT * FROM flags" , (err, res) => {
-  if (err){
+db.query("SELECT * FROM flags", (err, res) => {
+  if (err) {
     console.error("Error executing query", err.stack);
   } else {
     quiz = res.rows;
   }
-
   db.end();
 });
-
 
 let totalCorrect = 0;
 
@@ -36,9 +34,9 @@ app.use(express.static("public"));
 let currentQuestion = {};
 
 // GET home page
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   totalCorrect = 0;
-  nextQuestion();
+  await nextQuestion();
   console.log(currentQuestion);
   res.render("index.ejs", { question: currentQuestion });
 });
@@ -47,7 +45,7 @@ app.get("/", (req, res) => {
 app.post("/submit", (req, res) => {
   let answer = req.body.answer.trim();
   let isCorrect = false;
-  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+  if (currentQuestion.name.toLowerCase() === answer.toLowerCase()) {
     totalCorrect++;
     console.log(totalCorrect);
     isCorrect = true;
@@ -61,11 +59,13 @@ app.post("/submit", (req, res) => {
   });
 });
 
-function nextQuestion() {
+async function nextQuestion() {
   const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+
   currentQuestion = randomCountry;
 }
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
