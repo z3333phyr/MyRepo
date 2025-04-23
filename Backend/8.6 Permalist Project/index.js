39ec8dcc77c7b8f67b8e9fff4956df23647d8cfa
z3333phyr <pagulayan.francis03@gmail.com>
2025-04-23 10:12:34 +0800

    permalist

---
+++ Backend/8.6 Permalist Project/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "thisismypassword",
+  port: 5432
+});
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = [
+  { id: 1, title: "Buy milk" },
+  { id: 2, title: "Finish homework" },
+];
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    listTitle: "Today",
+    listItems: items,
+  });
+});
+
+app.post("/add", (req, res) => {
+  const item = req.body.newItem;
+  items.push({ title: item });
+  res.redirect("/");
+});
+
+app.post("/edit", (req, res) => {});
+
+app.post("/delete", (req, res) => {});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
