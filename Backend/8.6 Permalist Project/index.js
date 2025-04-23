4570b78ce66b2f91ef3385665ae60fd822e2ff59
z3333phyr <pagulayan.francis03@gmail.com>
2025-04-23 10:50:41 +0800

    insert to db

--- Backend/8.6 Permalist Project/index.js
+++ Backend/8.6 Permalist Project/index.js
@@ -21,11 +21,19 @@ let items = [
   { id: 2, title: "Finish homework" },
 ];
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    listTitle: "Today",
-    listItems: items,
-  });
+app.get("/",  async (req, res) => {
+  try{
+    const result = await db.query("SELECT * FROM items ORDER BY id ASC ");
+    items = result.rows;
+
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listItems: items,
+    });
+  }catch (err){
+    console.log(err);
+  }
+  
 });
 
 app.post("/add", (req, res) => {
