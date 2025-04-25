159b32b817dfc612422dac9bf6237d3a2af49476
z3333phyr <pagulayan.francis03@gmail.com>
2025-04-25 16:26:45 +0800

    imported contacts

--- Backend/296-react-props-practice/src/components/App.jsx
+++ Backend/296-react-props-practice/src/components/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Card from "Card";
+import contacts from "../contacts";
 
 function App() {
+ 
   return (
     <div>
       <h1 className="heading">My Heading</h1>
       <Card 
-      name = "Beyonce"
-      img = "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" alt="avatar_img"
-      tel = "+123 456 789"
-      email = "b@beyonce.com"
+      name = {contacts[0].name}
+      img = {contacts[0].imgURL}
+      tel = {contacts[0].phone}
+      email = {contacts[0].email}
       /> 
     </div>
   );
