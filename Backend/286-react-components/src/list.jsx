8bb15bbb5e14415a2a457d954424eeb96ea9952a
z3333phyr <pagulayan.francis03@gmail.com>
2025-04-25 13:45:10 +0800

    modified

--- Backend/286-react-components/src/list.jsx
+++ Backend/286-react-components/src/list.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function List (){
     return (
     <ul>
-    <li>Bacon</li>
-    <li>Jamon</li>
-    <li>Noodles</li>
+        <li>Bacon</li>
+        <li>Jamon</li>
+        <li>Noodles</li>
   </ul>);
 }
 export default List;
