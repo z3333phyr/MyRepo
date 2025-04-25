8cf113e9a6b1b3f651d2194792c15565a145af7a
z3333phyr <pagulayan.francis03@gmail.com>
2025-04-25 14:53:01 +0800

    import react

--- Backend/289-es6-import-export-practice/src/index.jsx
+++ Backend/289-es6-import-export-practice/src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {add, multiply, subtract, divide} from "./calculator";
+import * as Calculator from "./calculator";
 
 //Import the add, multiply, subtract and divide functions
 //from the calculator.js file.
@@ -8,10 +8,10 @@ import {add, multiply, subtract, divide} from "./calculator";
 
 ReactDOM.render(
   <ul>
-    <li>{add(1, 2)}</li>
-    <li>{multiply(2, 3)}</li>
-    <li>{subtract(7, 2)}</li>
-    <li>{divide(5, 2)}</li>
+    <li>{Calculator.add(1, 2)}</li>
+    <li>{Calculator.multiply(2, 3)}</li>
+    <li>{Calculator.subtract(7, 2)}</li>
+    <li>{Calculator.divide(5, 2)}</li>
   </ul>,
   document.getElementById("root")
 );
