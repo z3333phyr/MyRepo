4c28643f8dcc2da1b7a3d9645494732791ca4a04
z3333phyr <pagulayan.francis03@gmail.com>
2025-04-25 12:40:36 +0800

    jsx3

---
+++ Backend/283-jsx-attributes-and-styling/src/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1 className="title">Cute dogs </h1>
+    <img className="dog-img" src="https://www.google.com/imgres?q=dogs&imgurl=https%3A%2F%2Fimages.theconversation.com%2Ffiles%2F625049%2Foriginal%2Ffile-20241010-15-95v3ha.jpg%3Fixlib%3Drb-4.1.0%26rect%3D12%252C96%252C2671%252C1335%26q%3D45%26auto%3Dformat%26w%3D1356%26h%3D668%26fit%3Dcrop&imgrefurl=https%3A%2F%2Ftheconversation.com%2Fthe-science-of-happier-dogs-5-tips-to-help-your-canine-friends-live-their-best-life-236952&docid=nVJvwXsRkyobqM&tbnid=kJZiNrh1U9EYBM&vet=12ahUKEwi97_XtpPKMAxVZzDgGHV8ABsoQM3oECGcQAA..i&w=1356&h=668&hcb=2&ved=2ahUKEwi97_XtpPKMAxVZzDgGHV8ABsoQM3oECGcQAA"/>
+    <img className="dog-img" src="https://www.google.com/imgres?q=dogs&imgurl=https%3A%2F%2Fhips.hearstapps.com%2Fhmg-prod%2Fimages%2Fbest-guard-dogs-1650302456.jpeg%3Fcrop%3D1xw%3A0.9983344437041972xh%3Bcenter%2Ctop%26resize%3D1200%3A*&imgrefurl=https%3A%2F%2Fwww.countryliving.com%2Flife%2Fkids-pets%2Fg39559653%2Fbest-guard-dogs%2F&docid=uAGdzCj0GeIV2M&tbnid=rpptSzoCw_s7KM&vet=12ahUKEwi97_XtpPKMAxVZzDgGHV8ABsoQM3oECB4QAA..i&w=1200&h=900&hcb=2&ved=2ahUKEwi97_XtpPKMAxVZzDgGHV8ABsoQM3oECB4QAA"/>
+    <img className="dog-img" src="https://www.google.com/imgres?q=dogs&imgurl=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0086%2F0795%2F7054%2Ffiles%2FGolden-Retriever.jpg%3Fv%3D1645179525&imgrefurl=https%3A%2F%2Fheadsupfortails.com%2Fblogs%2Fdogs%2Fdog-breeds-in-india&docid=UDikhV011hN-mM&tbnid=epam15qv1IeTaM&vet=12ahUKEwi97_XtpPKMAxVZzDgGHV8ABsoQM3oECGgQAA..i&w=800&h=800&hcb=2&itg=1&ved=2ahUKEwi97_XtpPKMAxVZzDgGHV8ABsoQM3oECGgQAA"/>
+
+  </div>,
+  document.getElementById("root")
+);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// className="dog-img" 
+// to launch your react project in your browser
