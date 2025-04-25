a3f9724d3fa752703fc486be11b312094d13e9bb
z3333phyr <pagulayan.francis03@gmail.com>
2025-04-25 13:38:54 +0800

    react comp

---
+++ Backend/286-react-components/src/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./heading";
+import List from "./list";
+
+ReactDOM.render(
+  <div>
+    <Heading />
+    <List />
+  </div>,
+  document.getElementById("root")
+);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
