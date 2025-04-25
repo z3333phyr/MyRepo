199504dfe470713334772f766de81aadf1e32cb7
z3333phyr <pagulayan.francis03@gmail.com>
2025-04-25 10:15:34 +0800

    jsx test

---
+++ Backend/280-jsx-code-challenge/src/index.jsx
@@ -0,0 +1,24 @@
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
